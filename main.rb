5b958fa5ba21d8c6af3ad0eb7a642e58bb37a9fa
karlley <karlley.jp@gmail.com>
2022-05-23 05:20:14 +0900

    一覧、作成機能を追加

---
+++ main.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+get '/' do
+  @memos = File.open('./db/memos.json', 'r') { |memos| JSON.load(memos) }
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  memo_data = File.open('./db/memos.json', 'r') { |memos| JSON.load(memos) }
+  memo_data['memos'] << { 'id': memo_data['memos'].last['id'].next,
+                          'title': params[:title],
+                          'content': params[:content] }
+  File.open('./db/memos.json', 'w') { |memos| JSON.dump(memo_data, memos) }
+  redirect '/'
+  erb :index
+end
